d2825035722020f20a88ec25738e3db5514825ee
lionel-prats <lionel.prats.c@gmail.com>
2024-08-24 18:21:49 -0300

    266. Instalando Firebase

--- src/main.js
+++ src/main.js
@@ -10,6 +10,11 @@ import * as components from 'vuetify/components' // importo los componentes de V
 import * as directives from 'vuetify/directives' // importo las directivas de Vuetify (v260)
 // fin importaciones Vuetify (v260)
 
+// Firebase (v266)
+import { VueFire, VueFireAuth } from "vuefire"
+import { firebaseApp } from "./config/firebase"
+// fin Firebase (v266)
+
 import App from './App.vue'
 import router from './router'
 
@@ -22,6 +27,16 @@ const vuetify = createVuetify({
 })
 app.use(vuetify)
 // fin configuracion Vuetify (v260)
+
+// configuracion Firebase (v266)
+app.use(
+    VueFire,
+    {
+        firebaseApp, // credenciales
+        modules: [VueFireAuth()]
+    }
+)
+// fin configuracion Firebase (v266)
 
 app.use(createPinia())
 app.use(router)
